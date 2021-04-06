b0347f5dda816e87c991c5e0c02cd7ef79d0de02
mallu <mallu@HSC-5YQM6Y2>
2021-04-06 09:45:58 +0530

    Partial Views

---
+++ MVCBasics/Controllers/HeaderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasics.Controllers
+{
+    public class HeaderController : Controller
+    {
+        public PartialViewResult Index()
+        {
+            ViewBag.Text = "This is a partial Header view daya coming from server";
+            return PartialView("_Header");
+        }
+    }
+}
