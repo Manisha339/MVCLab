5e67f390469fa6c273a118afa62cc30bdd7dc5d0
mallu <mallu@HSC-5YQM6Y2>
2021-04-09 12:14:31 +0530

    Viewform

---
+++ FormsDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="FormsDemo.Global_asax" Language="vb" %>
---
+++ FormsDemo1/State.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FormsDemo1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        int counter
+        {
+            get
+            {
+                if(ViewState["counter"] is null)
+                {
+                    return 0;
+                }
+                return (int)ViewState["counter"];
+            }
+            set
+            {
+                ViewState["counter"] = value;
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            lblCounter.Text = counter.ToString();
+            counter++;
+        }
+    }
+}
